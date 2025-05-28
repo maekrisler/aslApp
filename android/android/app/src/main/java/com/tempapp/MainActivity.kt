ebfbea5c3f6c991d0de96248d5610b9b00975e84
Mae Krisler <mae@bkds.com>
2025-05-28 14:56:37 +0200

    added expo dependencys

--- android/android/app/src/main/java/com/tempapp/MainActivity.kt
+++ android/android/app/src/main/java/com/tempapp/MainActivity.kt
@@ -1,5 +1,4 @@
-package com.aslapp
-import expo.modules.ReactActivityDelegateWrapper
+package com.tempapp
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
@@ -12,12 +11,12 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "aslApp"
+  override fun getMainComponentName(): String = "TempApp"
 
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
-      ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED, DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled))
+      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
